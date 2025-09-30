fda834371a7be44ff058d174fb04c6844cedbc65
keshava-in-me <kantkeshava16@gmail.com>
2025-09-30 23:45:20 +0530

    final changes

---
+++ index.js
@@ -0,0 +1,23 @@
+function Play() {
+  randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  document
+    .querySelector(".img1")
+    .setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+  randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  document
+    .querySelector(".img2")
+    .setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+  if (randomNumber1 == randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 wins";
+  }
+
+  if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins🚩";
+  }
+}
